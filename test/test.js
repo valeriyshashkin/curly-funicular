d13da0f1109df6d35180964545379c03475ed49e
valeriyshashkin <velianjanjan@gmail.com>
2021-03-02 20:47:31 +0500

    add empty tests

---
+++ test/test.js
@@ -0,0 +1,8 @@
+describe('Select the active menu item when scrolling', function() {
+    it('selects the same menu item when not scrolled enough', function() {
+
+    });
+    it('selects the next menu item when scrolled enough', function() {
+
+    });
+});
