97529b75aa07b659f38c7a5f8d212445f9174268
valeriyshashkin <velianjanjan@gmail.com>
2021-03-03 18:30:44 +0500

    add library code

--- dist/curly-funicular.js
+++ dist/curly-funicular.js
@@ -1,18 +1,65 @@
 (function(window) {
-    let menu;
     let anchors;
-    let offset = 0;
+    let offset;
+    let previousAnchor;
 
-    window.curlyFunicular = {
-        init(config) {
-            Object.assign({menu, anchors, offset}, config);
+    window.curlyFunicular = function (config) {
+        ({anchors, offset} = config);
+        offset = offset || 0;
+        previousAnchor = anchors[0];
 
-            this.listenEvent();
-        },
+        if (!window.location.hash) {
+            window.location.hash = anchors[0];
+        }
 
-        listenEvent() {
-            
+        selectMenuItem(window.location.hash);
+        handleClick();
+        listenEvent();
+    }
+
+    function listenEvent() {
+        window.addEventListener('scroll', function() {
+            let isMenuChanged = false;
+            for (let i = 0; i < anchors.length - 1; i++) {
+                let topElement = `[data-cfanchor="${anchors[i].replace('#', '')}"]`;
+                let bottomElement = `[data-cfanchor="${anchors[i + 1].replace('#', '')}"]`;
+                if (
+                    window.pageYOffset >= document.querySelector(topElement).pageYOffset - offset &&
+                    window.pageYOffset < document.querySelector(bottomElement).pageYOffset - offset
+                ) {
+                    isMenuChanged = true;
+                    selectMenuItem(anchors[i]);
+                }
+            }
+
+            if (!isMenuChanged) {
+                selectMenuItem(anchors[anchors.length - 1]);
+            }
+        });
+    }
+
+    function selectMenuItem(anchor) {
+        if (anchor === previousAnchor) {
+            return;
+        }
+
+        previousAnchor = anchor;
+        window.location.hash = anchor;
+        
+        let menuItems = document.querySelectorAll('[data-cfmenuanchor]');
+        for (let i = 0; i < menuItems.length; i++) {
+            menuItems[i].classList.remove('cfactive');
+            if (menuItems[i].dataset.cfmenuanchor === anchor.replace('#', '')) {
+                menuItems[i].classList.add('cfactive');
+            }
         }
     }
 
+    function handleClick() {
+        let menuItems = document.querySelectorAll(`[data-cfmenuanchor]`);
+        for (let i = 0; i < menuItems.length; i++) {
+            let target = `[data-cfanchor="${menuItems[i].dataset.cfmenuanchor}"]`;
+            window.scrollTo(document.querySelector(target));
+        }
+    }
 })(window);
--- test/test.js
+++ test/test.js
@@ -7,59 +7,55 @@ describe('Select the active menu item when scrolling', function () {
     });
     describe('Without offset', function () {
         it('selects the same menu item when not scrolled enough', function () {
-            curlyFunicular.init({
-                menu: '#menu',
+            curlyFunicular({
                 anchors: ['#header', '#works', '#contacts']
             });
 
-            $(window).scrollTop($(document).find('[data-anchor]').height() - 1);
-            chai.assert.equal($('#menu').find('.active').index(), 0);
+            $(window).scrollTop($(document).find('[data-cfanchor]').height() - 1);
+            chai.assert.equal($(document).find('.cfactive').index(), 0);
 
-            $(window).scrollTop($(document).find('[data-anchor]').height());
-            chai.assert.equal($('#menu').find('.active').index(), 1);
+            $(window).scrollTop($(document).find('[data-cfanchor]').height());
+            chai.assert.equal($(document).find('.cfactive').index(), 1);
         });
         it('selects the next menu item when scrolled enough', function () {
-            curlyFunicular.init({
-                menu: '#menu',
+            curlyFunicular({
                 anchors: ['#header', '#works', '#contacts']
             });
 
             for (let i = 1; i < 3; i++) {
-                $(window).scrollTop($(document).find('[data-anchor]').height() * i - 1);
-                chai.assert.equal($('#menu').find('.active').index(), i - 1);
+                $(window).scrollTop($(document).find('[data-cfanchor]').height() * i - 1);
+                chai.assert.equal($(document).find('.cfactive').index(), i - 1);
 
-                $(window).scrollTop($(document).find('[data-anchor]').height() * i);
-                chai.assert.equal($('#menu').find('.active').index(), i);
+                $(window).scrollTop($(document).find('[data-cfanchor]').height() * i);
+                chai.assert.equal($(document).find('.cfactive').index(), i);
             }
         });
     });
     describe('With offset', function () {
         it('selects the same menu item when not scrolled enough with offset', function () {
-            curlyFunicular.init({
-                menu: 'menu',
+            curlyFunicular({
                 anchors: ['#header', '#works', '#contacts'],
                 offset: 200
             });
 
-            $(window).scrollTop($(document).find('[data-anchor]').height() - 201);
-            chai.assert.equal($('#menu').find('.active').index(), 0);
+            $(window).scrollTop($(document).find('[data-cfanchor]').height() - 201);
+            chai.assert.equal($(document).find('.cfactive').index(), 0);
 
-            $(window).scrollTop($(document).find('[data-anchor]').height() - 200);
-            chai.assert.equal($('#menu').find('.active').index(), 1);
+            $(window).scrollTop($(document).find('[data-cfanchor]').height() - 200);
+            chai.assert.equal($(document).find('.cfactive').index(), 1);
         });
         it('selects the next menu item when scrolled enough with offset', function () {
-            curlyFunicular.init({
-                menu: 'menu',
+            curlyFunicular({
                 anchors: ['#header', '#works', '#contacts'],
                 offset: 200
             });
 
             for (let i = 1; i < 3; i++) {
-                $(window).scrollTop($(document).find('[data-anchor]').height() * i - 201);
-                chai.assert.equal($('#menu').find('.active').index(), i - 1);
+                $(window).scrollTop($(document).find('[data-cfanchor]').height() * i - 201);
+                chai.assert.equal($(document).find('.cfactive').index(), i - 1);
 
-                $(window).scrollTop($(document).find('[data-anchor]').height() * i - 200);
-                chai.assert.equal($('#menu').find('.active').index(), i);
+                $(window).scrollTop($(document).find('[data-cfanchor]').height() * i - 200);
+                chai.assert.equal($(document).find('.cfactive').index(), i);
             }
         });
     });
