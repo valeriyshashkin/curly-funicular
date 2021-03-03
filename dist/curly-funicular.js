e59505e3d923785d66c2c6a7b029278fa813d954
valeriyshashkin <velianjanjan@gmail.com>
2021-03-03 10:37:38 +0500

    change library from class to iife

--- dist/curly-funicular.js
+++ dist/curly-funicular.js
@@ -1,7 +1,18 @@
-class CurlyFunicular {
-    constructor(config) {
-        this.menu = config.menu;
-        this.anchors = config.anchors;
-        this.offset = config.offset || 0;
+(function(window) {
+    let menu;
+    let anchors;
+    let offset = 0;
+
+    window.curlyFunicular = {
+        init(config) {
+            Object.assign({menu, anchors, offset}, config);
+
+            this.listenEvent();
+        },
+
+        listenEvent() {
+            
+        }
     }
-}
+
+})(window);
--- test/test.js
+++ test/test.js
@@ -1,25 +1,30 @@
 describe('Select the active menu item when scrolling', function() {
     it('selects the same menu item when not scrolled enough', function() {
-        let curlyFunicular = new CurlyFunicular({
-            menu: 'menu',
+        curlyFunicular.init({
+            menu: `#menu`,
             anchors: ['#header', '#works', '#contacts'],
         });
-        $(window).scrollTop();
+
+        $(window).scrollTop(0);
+        chai.assert.equal($('#menu').find('.active').index(), 0);
+
+        $(window).scrollTop($('#header').clientHeight / 2);
+        chai.assert.equal($('#menu').find('.active').index(), 0);
     });
     it('selects the next menu item when scrolled enough', function() {
-        let curlyFunicular = new CurlyFunicular({
+        curlyFunicular.init({
             menu: 'menu',
             anchors: ['#header', '#works', '#contacts'],
         });
         $(window).scrollTop(0);
-        assert.equal($('#desktop-panel').find('.active[data-menuanchor="header"]').index(), 0);
+        chai.assert.equal($('#desktop-panel').find('.active').index(), 0);
         for (let i = 1; i < 3; i++) {
-            $(window).scrollTop();
-            assert.equal($('#desktop-panel').find('.active[data-menuanchor=""]').index(), i);
+            $(window).scrollTop($('#header').clientHeight * i);
+            chai.assert.equal($('#desktop-panel').find('.active').index(), i);
         }
     });
     it('selects the same menu item when not scrolled enough with offset', function() {
-        let curlyFunicular = new CurlyFunicular({
+        curlyFunicular.init({
             menu: 'menu',
             anchors: ['#header', '#works', '#contacts'],
             offset: 200
@@ -27,16 +32,16 @@ describe('Select the active menu item when scrolling', function() {
         $(window).scrollTop();
     });
     it('selects the next menu item when scrolled enough with offset', function() {
-        let curlyFunicular = new CurlyFunicular({
+        curlyFunicular.init({
             menu: 'menu',
             anchors: ['#header', '#works', '#contacts'],
             offset: 200
         });
         $(window).scrollTop(0);
-        assert.equal($('#desktop-panel').find('.active[data-menuanchor="header"]').index(), 0);
+        chai.assert.equal($('#desktop-panel').find('.active[data-menuanchor="header"]').index(), 0);
         for (let i = 1; i < 3; i++) {
             $(window).scrollTop();
-            assert.equal($('#desktop-panel').find('.active[data-menuanchor=""]').index(), i);
+            chai.assert.equal($('#desktop-panel').find('.active[data-menuanchor=""]').index(), i);
         }
     });
 });
