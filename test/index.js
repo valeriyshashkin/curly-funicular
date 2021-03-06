35d9208e47dd0e13739d1683d073f454dc33f889
valeriyshashkin <velianjanjan@gmail.com>
2021-03-06 14:55:46 +0500

    outer hooks

--- test/index.js
+++ test/index.js
@@ -2,64 +2,65 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 const { assert } = require('chai');
 
+let browser;
+let page;
+
+before(async function () {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto(path.join('file://', __dirname, '/index.html'));
+});
+
+after(async function () {
+    await page.close();
+    browser.close();
+});
+
 describe('Select the active menu item when scrolling', function () {
-    let browser;
-    let page;
-
-    before(async function () {
-        browser = await puppeteer.launch();
-        page = await browser.newPage();
-        await page.goto(path.join('file://', __dirname, '/index.html'));
-    });
-
-    after(async function () {
-        await page.close();
-        browser.close();
-    });
-
-    // describe('Without offset', function () {
-    it('selects the same menu item when not scrolled enough', async function () {
-        await page.evaluate(() => {
-            curlyFunicular({
-                menu: '#menu',
-                anchors: ['#header', '#works', '#contacts']
+    describe('Without offset', function () {
+        it('selects the same menu item when not scrolled enough', async function () {
+            await page.evaluate(() => {
+                curlyFunicular({
+                    menu: '#menu',
+                    anchors: ['#header', '#works', '#contacts']
+                });
             });
-        });
 
-        await page.evaluate(() => {
-            window.scrollTo(0, window.document.querySelector('[data-cfanchor]').offsetHeight - 1);
-        });
-        let result = await page.$eval('.cfactive', el => {
-            function indexInParent(node) {
-                var children = node.parentNode.childNodes;
-                var num = 0;
-                for (var i = 0; i < children.length; i++) {
-                    if (children[i] == node) return num;
-                    if (children[i].nodeType == 1) num++;
+            await page.evaluate(() => {
+                window.scrollTo(0, window.document.querySelector('[data-cfanchor]').offsetHeight - 1);
+            });
+            let result = await page.$eval('.cfactive', el => {
+                function indexInParent(node) {
+                    var children = node.parentNode.childNodes;
+                    var num = 0;
+                    for (var i = 0; i < children.length; i++) {
+                        if (children[i] == node) return num;
+                        if (children[i].nodeType == 1) num++;
+                    }
+                    return -1;
                 }
-                return -1;
-            }
-            return indexInParent(el);
-        });
-        assert.equal(result, 0);
+                return indexInParent(el);
+            });
+            assert.equal(result, 0);
 
-        await page.evaluate(() => {
-            window.scrollTo(0, window.document.querySelector('[data-cfanchor]').offsetHeight);
-        });
-        await page.waitForSelector('.cfactive[data-cfmenuanchor="works"]');
-        result = await page.$eval('.cfactive', el => {
-            function indexInParent(node) {
-                var children = node.parentNode.childNodes;
-                var num = 0;
-                for (var i = 0; i < children.length; i++) {
-                    if (children[i] == node) return num;
-                    if (children[i].nodeType == 1) num++;
+            await page.evaluate(() => {
+                window.scrollTo(0, window.document.querySelector('[data-cfanchor]').offsetHeight);
+            });
+            await page.waitForSelector('.cfactive[data-cfmenuanchor="works"]');
+            result = await page.$eval('.cfactive', el => {
+                function indexInParent(node) {
+                    var children = node.parentNode.childNodes;
+                    var num = 0;
+                    for (var i = 0; i < children.length; i++) {
+                        if (children[i] == node) return num;
+                        if (children[i].nodeType == 1) num++;
+                    }
+                    return -1;
                 }
-                return -1;
-            }
-            return indexInParent(el);
+                return indexInParent(el);
+            });
+            assert.equal(result, 1);
         });
-        assert.equal(result, 1);
     });
     // it('selects the next menu item when scrolled enough', function () {
     //     curlyFunicular({
